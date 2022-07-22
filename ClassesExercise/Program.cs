ccb5758b4bfc362702de1ee48858cc10268b38c8
Nate <nathanial.lunceford@gmail.com>
2022-07-22 13:57:04 -0500

    Initial Commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using ClassesExercise;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Car newCar = new Car();
+        newCar.Make = "Subaru";
+        newCar.Model = "Forester";
+        newCar.Year = "2007";
+
+        Console.WriteLine($"My car is a {newCar.Make} {newCar.Model}, from the " +
+            $"year {newCar.Year}");    
+    }
+}
